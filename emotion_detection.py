c8b0cbfda096754eac703ab279c746c12dade43e
Niraj <nkj5@uakron.edu>
2026-02-12 17:59:07 -0500

    Add emotion detection file

---
+++ emotion_detection.py
@@ -0,0 +1,9 @@
+# In the emotion_detection.py file, write the function to run emotion detection using 
+# the appropriate Emotion Detection function. Name this function emotion_detector.
+
+# Note: Assume that that text to be analyzed is passed to the function as an argument 
+# and is stored in the variable text_to_analyze. The value being returned must be the 
+# text attribute of the response object as received from the Emotion Detection function.
+
+def emotion_detector():
+    pass
