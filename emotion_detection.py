2fb056e44f528d00973f0559a8a293804b27cb06
Niraj <nkj5@uakron.edu>
2026-02-12 19:04:38 -0500

    Add dominant emotion feature

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,9 +1,21 @@
-# In the emotion_detection.py file, write the function to run emotion detection using 
-# the appropriate Emotion Detection function. Name this function emotion_detector.
+import requests
+import json
 
-# Note: Assume that that text to be analyzed is passed to the function as an argument 
-# and is stored in the variable text_to_analyze. The value being returned must be the 
-# text attribute of the response object as received from the Emotion Detection function.
+# define emotion detection using Emotion Predict function of the Watson NLP Library
+def emotion_detector(text_to_analyse):
+    # url for emotion predict service
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    # set a header requirements
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    # create a dictionary with the text to be analyzed
+    obj = { "raw_document": { "text": text_to_analyse } }
 
-def emotion_detector():
-    pass
+    # Send post request to the API with text and header
+    response = requests.post(url, json = obj, headers = headers )
+
+    # Return the response from the API
+    formatted_response = json.loads(response.text)
+
+    return (formatted_response)
+
+emotion_detector('The Weather is too good. I feel Like going to play soccer.')
